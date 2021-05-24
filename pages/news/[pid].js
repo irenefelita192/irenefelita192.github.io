91cc8ce2f5891d185590e82c2169380a65b41e18
IRENE <irene@IRENEs-MacBook-Air.local>
2021-05-25 00:24:12 +0700

    Add staticpath for dynamic route

--- pages/news/[pid].js
+++ pages/news/[pid].js
@@ -1,10 +1,34 @@
 import Layout from '../../components/layout'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
+import { getNews } from '../../services/news'
 import NewsDetailScreen from '../../screens/news-detail'
 
-export default function Career() {
-    const router = useRouter(),
-        newsId = router.query?.pid ?? null
+export async function getStaticPaths() {
+    const newsData = await getNews()
+
+    return {
+        paths:
+            newsData &&
+            newsData.map((dt) => {
+                return {
+                    params: {
+                        pid: `${dt.id}`,
+                    },
+                }
+            }),
+        fallback: false,
+    }
+}
+
+export async function getStaticProps({ params }) {
+    return {
+        props: { newsId: params?.pid ?? '' },
+    }
+}
+
+export default function Career({ newsId }) {
+    // const router = useRouter(),
+    //     newsId = router.query?.pid ?? null
 
     return (
         <Layout>
