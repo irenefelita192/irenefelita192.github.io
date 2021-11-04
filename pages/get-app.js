2dc97e5237e3a0fb98d03ae3e55ca72d8dea7c51
IRENE <irenefelita92@gmail.com>
2021-11-04 19:42:37 +0700

    fix: /get-app desktop link to home vida, ios and android link to correct link

--- pages/get-app.js
+++ pages/get-app.js
@@ -6,24 +6,46 @@ import Loader from 'components/loader'
 
 export default function GetApp() {
     useAsyncEffect(async (isMounted) => {
-        let langId, paramLocale
+        let langId,
+            paramLocale,
+            ios = false,
+            isDesktop = true
         if (process.browser) {
             const urlParams = new URLSearchParams(
                 window?.location?.search ?? ''
             )
             paramLocale = urlParams.get('locale')
             langId = getCookie('lang')
+
+            ios = /(iPad|iPhone|iPod)/g.test(navigator.userAgent)
+            if (window.innerWidth < window.innerHeight) {
+                isDesktop = false
+            }
         }
         const footerDt = await getFooter(paramLocale || langId)
-        let ios = false
-        if (process.browser) {
-            ios = /(iPad|iPhone|iPod|Mac OS)/g.test(navigator.userAgent)
-        }
-
+        // window.open("market://details?id=com.your.app","_system")
         if (!isMounted()) return
         // setFooterData(footerDt)
-        if (ios) window.location.href = ifooterDt.AppStoreLink
-        else if (!ios) window.location.href = footerDt.PlayStoreLink
+        if (isDesktop) {
+            window.location.href = footerDt?.homeUrl ?? 'https://kenalvida.com/'
+        } else {
+            if (ios)
+                window.location.href =
+                    footerDt?.AppStoreLink ?? 'https://kenalvida.com/'
+            else if (!ios) {
+                const playStoreLink = footerDt
+                    ? footerDt.PlayStoreLink.replace(
+                          'https://play.google.com/store/apps/',
+                          'market://'
+                      )
+                    : ''
+                if (playStoreLink) {
+                    window.open(playStoreLink, '_system')
+                } else {
+                    window.location.href = 'https://kenalvida.com/'
+                }
+            }
+        }
     }, [])
 
     return <Loader />
