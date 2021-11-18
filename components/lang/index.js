4a78eb09851dc2b6f962ca9c4dfcc61ab97a93a7
IRENE <irenefelita92@gmail.com>
2021-11-18 17:08:25 +0700

    fix: remove old cookie at domain

--- components/lang/index.js
+++ components/lang/index.js
@@ -32,6 +32,7 @@ export default function LangPopup({ isDesktop }) {
 
     useEffect(() => {
         if (process.browser) {
+            document.cookie = `lang=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;`
             const langId = getCookie('lang')
             if (langId) setActiveLang(langId)
 
