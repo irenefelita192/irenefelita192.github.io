c70680cbfd7a7b3f9e43bc4b00331eea78319240
irenefelita192 <42824601+irenefelita192@users.noreply.github.com>
2023-02-05 08:24:02 +0000

    Deploying to gh-pages from @ irenefelita192/irenefelita192.github.io@8363cb01329a89df326313d610d3c12134d7aa5b 🚀

--- components/global-styles/global-styles.js
+++
@@ -1,8 +0,0 @@
-import css from 'styled-jsx/css'
-
-export default css.global`
-    * {
-        margin: 0;
-        padding: 0;
-    }
-`
--- pdf-worker.js
+++
@@ -1,6 +0,0 @@
-if (process.env.nodeEnv === 'production') {
-    // use minified verion for production
-    module.exports = require('pdfjs-dist/build/pdf.worker.min.js')
-} else {
-    module.exports = require('pdfjs-dist/build/pdf.worker.js')
-}
