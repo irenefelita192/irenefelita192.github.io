9f39112796ec7d53602165f15b3332d8f313228d
IRENE <irenefelita92@gmail.com>
2023-02-05 14:10:32 +0700

    feat: init commit kids project

--- components/extended-table/pagination-total/styles.js
+++
@@ -1,8 +0,0 @@
-import theme from 'components/global-styles/theme'
-import css from 'styled-jsx/css'
-
-export default css`
-    .pagination-total.is-mobile {
-        margin-bottom: 60px;
-    }
-`
--- pages/_app.js
+++
@@ -1,5 +0,0 @@
-import 'normalize.css/normalize.css'
-
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
